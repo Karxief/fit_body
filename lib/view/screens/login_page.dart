9e095b042e70b9e24a17769cd056d3402f8c7cfa
Karxief <karxief@gmail.com>
2022-10-04 01:36:48 +0300

    login page v2

--- lib/view/screens/login_page.dart
+++ lib/view/screens/login_page.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:fit_body/view/widgets/EmailTextField_widgets.dart';
 import 'package:fit_body/view/widgets/PasswordTextfield_witget.dart';
 import 'package:fit_body/view/widgets/backround_image_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../widgets/Buttons/buttons_witgets.dart';
 import '../widgets/welcome_text.dart';
 
 class LoginPage extends StatefulWidget {
@@ -39,38 +42,61 @@ class _LoginPageState extends State<LoginPage> {
                         ]),
                       ),
                     )),
-                const Expanded(
+                Expanded(
                     flex: 1,
                     child: EmailTextField(
                         // loginpagemailcontroller: null,
                         )),
-                const SizedBox(height: 10),
-                const Expanded(flex: 1, child: PasswordTextField()),
-                const Text('Forgot Password',
-                    style: TextStyle(
-                      fontFamily: 'Dosis',
-                      color: Color.fromARGB(255, 173, 223, 145),
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                    )),
-
+                SizedBox(height: 10),
+                Expanded(flex: 1, child: PasswordTextField()),
+                Expanded(
+                  flex: 1,
+                  child: Text('Forgot Password',
+                      style: Theme.of(context).textTheme.bodyText2),
+                ),
                 // SizedBox(height: 5,),
+                Expanded(
+                  flex: 2,
+                  child: Row(
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: button_widget(
+                              icon_name: FaIcon(FontAwesomeIcons.google))),
+                      Expanded(
+                          flex: 1,
+                          child: button_widget(
+                            icon_name: FaIcon(FontAwesomeIcons.apple),
+                          )),
+                      Expanded(
+                          flex: 2,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Theme.of(context).primaryColor),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Login",
+                                  style: Theme.of(context).textTheme.headline1,
+                                ),
+                                Icon(Icons.arrow_right)
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
                 Expanded(
                     flex: 1,
                     child: Row(
                       children: [
-                        Expanded(
-                          child: Container(
-                            height: 50,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Theme.of(context).primaryColor),
-                            child: const Icon(
-                              Icons.apple,
-                              color: Colors.white,
-                            ),
-                          ),
+                        Text(
+                          "Don't you have an account?",
+                          style: Theme.of(context).textTheme.bodyText1,
                         ),
+                        TextButton(onPressed: () {}, child: Text('Sign in'))
                       ],
                     ))
               ],
